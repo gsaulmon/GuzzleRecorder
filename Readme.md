bc2b701e4c8cfb1828a7b8de32cbe14712c44322
gsaulmon <saulmon@gmail.com>
2015-04-06 01:52:18 -0400

    A few basic tests. Also made the Guzzle requirement less strict.

---
+++ Readme.md
@@ -0,0 +1,22 @@
+GuzzleRecorder
+==============
+
+This is an event listener plugin for Guzzle 4/5 aimed at setting up tests for complex responses a bit quicker.
+
+### Usage
+
+Attach an instance of the recorder to any Guzzle Client
+
+```php
+$client = new GuzzleHttp\Client();
+
+$watcher = new Gsaulmon\GuzzleRecorder\GuzzleRecorder(__DIR__ . '/my_test_responses');
+$client->getEmitter()->attach($watcher);
+
+```
+
+Then run your tests. The recorder will grab and store all Guzzle responses in the "my_test_responses" folder.
+All subsequent runs of the tests will have the Guzzle requests intercepted and will be injected with the stored response.
+
+
+*The responses store will be the full actual response. Make sure to edit out any information you may not want to share.*
---
+++ tests/GuzzleRecorderTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use \Gsaulmon\GuzzleRecorder\GuzzleRecorder;
+use GuzzleHttp\Event\BeforeEvent;
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Transaction;
+
+class GuzzleRecorderTest extends PHPUnit_Framework_TestCase
+{
+    /** @var GuzzleHttp\Client $client */
+    private $client = null;
+    private $recorder = null;
+
+    public function setup()
+    {
+        $this->client = new GuzzleHttp\Client([
+            'defaults' => [
+                'headers' => [
+                    'User-Agent' => 'GuzzleRecorder'
+                ]
+            ]
+        ]);
+
+        $this->recorder = new GuzzleRecorder(__DIR__ . '/responses');
+        $this->client->getEmitter()->attach($this->recorder);
+    }
+
+    /** @test */
+    public function test_getPath()
+    {
+        $request = new Request('GET', 'http://google.com');
+
+        $m = new ReflectionMethod($this->recorder, 'getPath');
+        $m->setAccessible(true);
+
+        $this->assertSame(__DIR__ . '/responses/get/google.com/', $m->invoke($this->recorder, $request));
+    }
+
+    /** @test */
+    public function test_getFileName()
+    {
+        $request = new Request('GET', 'http://google.com', [
+            'myheader' => 'myvalue',
+            'Cookie' => 'foo'
+        ]);
+
+        $m = new ReflectionMethod($this->recorder, 'getFileName');
+        $m->setAccessible(true);
+
+        $this->assertSame('f7c1e7964485802fe8842827a8ad7823.txt', $m->invoke($this->recorder, $request));
+    }
+
+    /** @test */
+    public function test_getFileName_excludes_cookies()
+    {
+        $this->recorder->includeCookies(false);
+
+        $request1 = new Request('GET', 'http://google.com', [
+            'myheader' => 'myvalue',
+            'Cookie' => 'foo'
+        ]);
+
+        $request2 = new Request('GET', 'http://google.com', [
+            'myheader' => 'myvalue'
+        ]);
+
+        $m = new ReflectionMethod($this->recorder, 'getFileName');
+        $m->setAccessible(true);
+
+        $this->assertSame('0ec133b60aaa14f35d2185c09e590a48.txt', $m->invoke($this->recorder, $request1));
+        $this->assertSame('0ec133b60aaa14f35d2185c09e590a48.txt', $m->invoke($this->recorder, $request2));
+    }
+
+}
